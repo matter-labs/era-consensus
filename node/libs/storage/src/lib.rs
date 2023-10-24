249df2b0d4d6abd54f8aa582084ff966131fce0c
Alex Ostrovski <aov@matterlabs.dev>
2023-10-24 15:48:37 +0300

    Remove `BufferedStorage`

--- node/libs/storage/src/lib.rs
+++ node/libs/storage/src/lib.rs
@@ -1,7 +1,6 @@
 //! This module is responsible for persistent data storage, it provides schema-aware type-safe database access. Currently we use RocksDB,
 //! but this crate only exposes an abstraction of a database, so we can easily switch to a different storage engine in the future.
 
-mod buffered;
 mod rocksdb;
 mod testonly;
 #[cfg(test)]
@@ -10,8 +9,7 @@ mod traits;
 mod types;
 
 pub use crate::{
-    buffered::BufferedStorage,
     rocksdb::RocksdbStorage,
-    traits::{BlockStore, ContiguousBlockStore, ReplicaStateStore, WriteBlockStore},
+    traits::{BlockStore, ReplicaStateStore, WriteBlockStore},
     types::{ReplicaState, StorageError, StorageResult},
 };
