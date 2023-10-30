7eb333aad55411cc015ee00591e88e6bd8da08f0
Alex Ostrovski <aov@matterlabs.dev>
2023-10-30 10:07:12 +0200

    Sketch in-memory block storage

---
+++ node/libs/storage/src/in_memory.rs
@@ -0,0 +1,137 @@
+//! In-memory storage implementation.
+
+use crate::{
+    traits::{BlockStore, WriteBlockStore},
+    types::MissingBlockNumbers,
+    StorageResult,
+};
+use async_trait::async_trait;
+use concurrency::{
+    ctx,
+    sync::{self, watch, Mutex},
+};
+use roles::validator::{BlockNumber, FinalBlock};
+use std::{collections::BTreeMap, ops};
+
+#[derive(Debug)]
+struct BlocksInMemoryStore {
+    blocks: BTreeMap<BlockNumber, FinalBlock>,
+    last_contiguous_block_number: BlockNumber,
+}
+
+impl BlocksInMemoryStore {
+    fn head_block(&self) -> &FinalBlock {
+        self.blocks.values().next_back().unwrap()
+        // ^ `unwrap()` is safe by construction; the storage contains at least the genesis block
+    }
+
+    fn first_block(&self) -> &FinalBlock {
+        self.blocks.values().next().unwrap()
+        // ^ `unwrap()` is safe by construction; the storage contains at least the genesis block
+    }
+
+    fn block(&self, number: BlockNumber) -> Option<&FinalBlock> {
+        self.blocks.get(&number)
+    }
+
+    fn missing_block_numbers(&self, range: ops::Range<BlockNumber>) -> Vec<BlockNumber> {
+        let existing_numbers = self
+            .blocks
+            .range(range.clone())
+            .map(|(&number, _)| Ok(number));
+        MissingBlockNumbers::new(range, existing_numbers)
+            .map(Result::unwrap)
+            .collect()
+    }
+
+    fn put_block(&mut self, block: FinalBlock) {
+        let block_number = block.header.number;
+        tracing::debug!("Inserting block #{block_number} into database");
+        if let Some(prev_block) = self.blocks.insert(block_number, block) {
+            tracing::debug!(?prev_block, "Block #{block_number} is overwritten");
+        } else {
+            for (&number, _) in self
+                .blocks
+                .range(self.last_contiguous_block_number.next()..)
+            {
+                let expected_block_number = self.last_contiguous_block_number.next();
+                if number == expected_block_number {
+                    self.last_contiguous_block_number = expected_block_number;
+                } else {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+/// In-memory store.
+#[derive(Debug)]
+pub struct InMemoryStore {
+    blocks: Mutex<BlocksInMemoryStore>,
+    blocks_sender: watch::Sender<BlockNumber>,
+}
+
+impl InMemoryStore {
+    /// Creates a new store containing only the specified `genesis_block`.
+    pub fn new(genesis_block: FinalBlock) -> Self {
+        let genesis_block_number = genesis_block.header.number;
+        Self {
+            blocks: Mutex::new(BlocksInMemoryStore {
+                blocks: BTreeMap::from([(genesis_block_number, genesis_block)]),
+                last_contiguous_block_number: genesis_block_number,
+            }),
+            blocks_sender: watch::channel(genesis_block_number).0,
+        }
+    }
+}
+
+#[async_trait]
+impl BlockStore for InMemoryStore {
+    async fn head_block(&self, ctx: &ctx::Ctx) -> StorageResult<FinalBlock> {
+        Ok(sync::lock(ctx, &self.blocks).await?.head_block().clone())
+    }
+
+    async fn first_block(&self, ctx: &ctx::Ctx) -> StorageResult<FinalBlock> {
+        Ok(sync::lock(ctx, &self.blocks).await?.first_block().clone())
+    }
+
+    async fn last_contiguous_block_number(&self, ctx: &ctx::Ctx) -> StorageResult<BlockNumber> {
+        Ok(sync::lock(ctx, &self.blocks)
+            .await?
+            .last_contiguous_block_number)
+    }
+
+    async fn block(
+        &self,
+        ctx: &ctx::Ctx,
+        number: BlockNumber,
+    ) -> StorageResult<Option<FinalBlock>> {
+        Ok(sync::lock(ctx, &self.blocks).await?.block(number).cloned())
+    }
+
+    async fn missing_block_numbers(
+        &self,
+        ctx: &ctx::Ctx,
+        range: ops::Range<BlockNumber>,
+    ) -> StorageResult<Vec<BlockNumber>> {
+        Ok(sync::lock(ctx, &self.blocks)
+            .await?
+            .missing_block_numbers(range))
+    }
+
+    fn subscribe_to_block_writes(&self) -> watch::Receiver<BlockNumber> {
+        self.blocks_sender.subscribe()
+    }
+}
+
+#[async_trait]
+impl WriteBlockStore for InMemoryStore {
+    async fn put_block(&self, ctx: &ctx::Ctx, block: &FinalBlock) -> StorageResult<()> {
+        sync::lock(ctx, &self.blocks)
+            .await?
+            .put_block(block.clone());
+        self.blocks_sender.send_replace(block.header.number);
+        Ok(())
+    }
+}
--- node/libs/storage/src/lib.rs
+++ node/libs/storage/src/lib.rs
@@ -1,6 +1,7 @@
 //! This module is responsible for persistent data storage, it provides schema-aware type-safe database access. Currently we use RocksDB,
 //! but this crate only exposes an abstraction of a database, so we can easily switch to a different storage engine in the future.
 
+mod in_memory;
 mod replica_state;
 mod rocksdb;
 mod testonly;
@@ -10,6 +11,7 @@ mod traits;
 mod types;
 
 pub use crate::{
+    in_memory::InMemoryStore,
     replica_state::FallbackReplicaStateStore,
     rocksdb::RocksdbStorage,
     traits::{BlockStore, ReplicaStateStore, WriteBlockStore},
