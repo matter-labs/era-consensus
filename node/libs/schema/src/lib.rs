25ab1b0e2aa2058d9c46d57b8388f8cf04c59ddd
Grzegorz Prusak <pompon.pompon@gmail.com>
2023-10-30 11:04:21 +0100

    snapshot

--- node/libs/schema/src/lib.rs
+++ node/libs/schema/src/lib.rs
@@ -13,11 +13,4 @@ mod tests;
 #[allow(warnings)]
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/proto/mod.rs"));
-    static DESCRIPTOR_POOL: once_cell::sync::Lazy<prost_reflect::DescriptorPool> =
-        once_cell::sync::Lazy::new(|| {
-            prost_reflect::DescriptorPool::decode(
-                include_bytes!(concat!(env!("OUT_DIR"), "/proto/descriptor.bin")).as_ref(),
-            )
-            .unwrap()
-        });
 }
