2345e62f33599d608f61991cce71792a30998ad0
Moshe Shababo <17073733+moshababo@users.noreply.github.com>
2023-10-30 19:44:59 -0500

    Generate random G1/G2 points directly

--- node/libs/crypto/src/bn254/testonly.rs
+++ node/libs/crypto/src/bn254/testonly.rs
@@ -1,7 +1,8 @@
 //! Random key generation, intended for use in testing
 
-use super::{AggregateSignature, PublicKey, SecretKey, Signature};
 use rand::{distributions::Standard, prelude::Distribution, Rng};
+
+use super::{AggregateSignature, PublicKey, SecretKey, Signature};
 
 /// Generates a random SecretKey. This is meant for testing purposes.
 impl Distribution<SecretKey> for Standard {
@@ -14,23 +15,20 @@ impl Distribution<SecretKey> for Standard {
 /// Generates a random PublicKey. This is meant for testing purposes.
 impl Distribution<PublicKey> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> PublicKey {
-        rng.gen::<SecretKey>().public()
+        rng.gen()
     }
 }
 
 /// Generates a random Signature. This is meant for testing purposes.
 impl Distribution<Signature> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Signature {
-        let key = rng.gen::<SecretKey>();
-        let msg = rng.gen::<[u8; 4]>();
-        key.sign(&msg)
+        rng.gen()
     }
 }
 
 /// Generates a random AggregateSignature. This is meant for testing purposes.
 impl Distribution<AggregateSignature> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> AggregateSignature {
-        let sig = rng.gen();
-        AggregateSignature(sig)
+        rng.gen()
     }
 }
