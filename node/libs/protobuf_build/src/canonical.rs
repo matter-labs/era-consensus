87bae087f59bc5198fc182bdbe6d1eca52059edc
Grzegorz Prusak <pompon.pompon@gmail.com>
2023-11-03 13:13:23 +0100

    snapshot

--- node/libs/protobuf_build/src/canonical.rs
+++ node/libs/protobuf_build/src/canonical.rs
@@ -1,29 +1,25 @@
 use anyhow::Context as _;
 use std::collections::HashSet;
+use super::proto;
 
 #[derive(Default)]
 struct Check(HashSet<String>);
 
 impl Check {
     /// Checks if messages of type `m` support canonical encoding.
-    fn message(&mut self, m: &prost_reflect::MessageDescriptor, check_nested: bool) -> anyhow::Result<()> { 
+    fn message(&mut self, m: prost_reflect::MessageDescriptor) -> anyhow::Result<()> { 
         if self.0.contains(m.full_name()) {
             return Ok(());
         }
         self.0.insert(m.full_name().to_string());
         for f in m.fields() {
-            self.field(&f).with_context(|| f.name().to_string())?;
-        }
-        if check_nested {
-            for m in m.child_messages() {
-                self.message(&m,check_nested).with_context(||m.name().to_string())?;
-            }
+            self.field(f).with_context(|| f.name().to_string())?;
         }
         Ok(())
     }
 
     /// Checks if field `f` supports canonical encoding.
-    fn field(&mut self, f: &prost_reflect::FieldDescriptor) -> anyhow::Result<()> {
+    fn field(&mut self, f: prost_reflect::FieldDescriptor) -> anyhow::Result<()> {
         if f.is_map() {
             anyhow::bail!("maps unsupported");
         }
@@ -31,27 +27,23 @@ impl Check {
             anyhow::bail!("non-repeated, non-oneof fields have to be marked as optional");
         }
         if let prost_reflect::Kind::Message(msg) = f.kind() {
-            self.message(&msg,false).with_context(||msg.name().to_string())?;
+            self.message(msg).with_context(||msg.name().to_string())?;
         }
         Ok(())
-    }
-
-    /// Checks if message types in file `f` support canonical encoding.
-    fn file(&mut self, f: &prost_reflect::FileDescriptor) -> anyhow::Result<()> {
-        if f.syntax() != prost_reflect::Syntax::Proto3 {
-            anyhow::bail!("only proto3 syntax is supported");
-        }
-        for m in f.messages() {
-            self.message(&m,true).with_context(|| m.name().to_string())?;
-        }
-        Ok(())
-    }
+    } 
 }
 
 pub fn check(descriptor: &prost_types::FileDescriptorSet, pool: &prost_reflect::DescriptorPool) -> anyhow::Result<()> {
-    let mut c = Check::default();
     for f in &descriptor.file {
-        c.file(f).with_context(||f.name())?;
+        if f.syntax() != "proto3" {
+            anyhow::bail!("only proto3 syntax is supported");
+        }
+    }
+    let mut c = Check::default();
+    for msg_name in proto::extract_message_names(descriptor) {
+        let msg_name = msg_name.to_string();
+        let msg = pool.get_message_by_name(&msg_name).with_context(||format!("{msg_name} not found in pool"))?;
+        c.message(msg).with_context(||msg_name)?;
     }
     Ok(())
 }
