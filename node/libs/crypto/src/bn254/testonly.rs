0d69e851ea314d8f942b5bf51d61f29d284c7dcf
Moshe Shababo <17073733+moshababo@users.noreply.github.com>
2023-11-08 21:04:32 -0600

    Create rand04::Rng from rand08::Rng seed as a workaround

--- node/libs/crypto/src/bn254/testonly.rs
+++ node/libs/crypto/src/bn254/testonly.rs
@@ -2,16 +2,15 @@
 
 use pairing::bn256::{Fr, G1, G2};
 use rand::{distributions::Standard, prelude::Distribution, Rng};
-use rand04::{thread_rng, Rand};
+use rand04::SeedableRng;
+use rand04::{Rand, StdRng as rand04StdRng};
 
 use super::{AggregateSignature, PublicKey, SecretKey, Signature};
 
 /// Generates a random SecretKey. This is meant for testing purposes.
 impl Distribution<SecretKey> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, _rng: &mut R) -> SecretKey {
-        // TODO: find a solution for rand::Rng trait object incompatibility with rand04::Rng trait bound
-        let mut rng = thread_rng();
-
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SecretKey {
+        let mut rng = rng08_to_rng04(rng);
         let scalar = Fr::rand(&mut rng);
         SecretKey(scalar)
     }
@@ -19,10 +18,8 @@ impl Distribution<SecretKey> for Standard {
 
 /// Generates a random PublicKey. This is meant for testing purposes.
 impl Distribution<PublicKey> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, _rng: &mut R) -> PublicKey {
-        // TODO: find a solution for rand::Rng trait object incompatibility with rand04::Rng trait bound
-        let mut rng = thread_rng();
-
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> PublicKey {
+        let mut rng = rng08_to_rng04(rng);
         let p = G2::rand(&mut rng);
         PublicKey(p)
     }
@@ -30,10 +27,8 @@ impl Distribution<PublicKey> for Standard {
 
 /// Generates a random Signature. This is meant for testing purposes.
 impl Distribution<Signature> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, _rng: &mut R) -> Signature {
-        // TODO: find a solution for rand::Rng trait object incompatibility with rand04::Rng trait bound
-        let mut rng = thread_rng();
-
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Signature {
+        let mut rng = rng08_to_rng04(rng);
         let p = G1::rand(&mut rng);
         Signature(p)
     }
@@ -41,11 +36,15 @@ impl Distribution<Signature> for Standard {
 
 /// Generates a random AggregateSignature. This is meant for testing purposes.
 impl Distribution<AggregateSignature> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, _rng: &mut R) -> AggregateSignature {
-        // TODO: find a solution for rand::Rng trait object incompatibility with rand04::Rng trait bound
-        let mut rng = thread_rng();
-
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> AggregateSignature {
+        let mut rng = rng08_to_rng04(rng);
         let p = G1::rand(&mut rng);
         AggregateSignature(p)
     }
+}
+
+fn rng08_to_rng04<R: Rng + ?Sized>(rng: &mut R) -> rand04StdRng {
+    let val = rng.gen::<usize>();
+    let seed: &[usize] = std::slice::from_ref(&val);
+    rand04::StdRng::from_seed(seed)
 }
