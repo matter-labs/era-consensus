f9f2291c292668fc2ed093a27d0ef0f98eaddb09
Alex Ostrovski <aov@matterlabs.dev>
2023-11-14 14:13:10 +0200

    Add basic tests for snapshot syncing

--- node/actors/sync_blocks/src/peers/tests.rs
+++ node/actors/sync_blocks/src/peers/tests.rs
@@ -26,6 +26,7 @@ struct TestHandles {
 #[async_trait]
 trait Test: fmt::Debug + Send + Sync {
     const BLOCK_COUNT: usize;
+    const GENESIS_BLOCK_NUMBER: usize = 0;
 
     fn tweak_config(&self, _config: &mut Config) {
         // Does nothing by default
@@ -91,7 +92,8 @@ async fn test_peer_states<T: Test>(test: T) {
     let ctx = &ctx::test_with_clock(ctx, &clock);
     let mut rng = ctx.rng();
     let test_validators = TestValidators::new(4, T::BLOCK_COUNT, &mut rng);
-    let storage = InMemoryStorage::new(test_validators.final_blocks[0].clone());
+    let storage =
+        InMemoryStorage::new(test_validators.final_blocks[T::GENESIS_BLOCK_NUMBER].clone());
     let storage = Arc::new(storage);
     test.initialize_storage(ctx, storage.as_ref(), &test_validators)
         .await;
@@ -176,6 +178,73 @@ impl Test for UpdatingPeerStateWithSingleBlock {
 #[tokio::test]
 async fn updating_peer_state_with_single_block() {
     test_peer_states(UpdatingPeerStateWithSingleBlock).await;
+}
+
+#[derive(Debug)]
+struct UpdatingPeerStateWithStorageSnapshot;
+
+#[async_trait]
+impl Test for UpdatingPeerStateWithStorageSnapshot {
+    const BLOCK_COUNT: usize = 5;
+    const GENESIS_BLOCK_NUMBER: usize = 2;
+
+    async fn test(self, ctx: &ctx::Ctx, handles: TestHandles) -> anyhow::Result<()> {
+        let TestHandles {
+            mut rng,
+            test_validators,
+            peer_states_handle,
+            storage,
+            mut message_receiver,
+            mut events_receiver,
+            clock,
+        } = handles;
+        let mut storage_subscriber = storage.subscribe_to_block_writes();
+
+        let peer_key = rng.gen::<node::SecretKey>().public();
+        for stale_block_number in [1, 2] {
+            peer_states_handle.update(
+                peer_key.clone(),
+                test_validators.sync_state(stale_block_number),
+            );
+            let peer_event = events_receiver.recv(ctx).await?;
+            assert_matches!(peer_event, PeerStateEvent::PeerUpdated(key) if key == peer_key);
+
+            // No new block requests should be issued.
+            clock.advance(BLOCK_SLEEP_INTERVAL);
+            sync::yield_now().await;
+            assert!(message_receiver.try_recv().is_none());
+        }
+
+        peer_states_handle.update(peer_key.clone(), test_validators.sync_state(3));
+        let peer_event = events_receiver.recv(ctx).await?;
+        assert_matches!(peer_event, PeerStateEvent::PeerUpdated(key) if key == peer_key);
+
+        // Check that the actor has sent a `get_block` request to the peer
+        let message = message_receiver.recv(ctx).await?;
+        let io::OutputMessage::Network(SyncBlocksInputMessage::GetBlock {
+            recipient,
+            number,
+            response,
+        }) = message;
+        assert_eq!(recipient, peer_key);
+        assert_eq!(number, BlockNumber(3));
+
+        // Emulate the peer sending a correct response.
+        test_validators.send_block(BlockNumber(3), response);
+
+        let peer_event = events_receiver.recv(ctx).await?;
+        assert_matches!(peer_event, PeerStateEvent::GotBlock(BlockNumber(3)));
+
+        // Check that the block has been saved locally.
+        let saved_block = *sync::changed(ctx, &mut storage_subscriber).await?;
+        assert_eq!(saved_block, BlockNumber(3));
+        Ok(())
+    }
+}
+
+#[tokio::test]
+async fn updating_peer_state_with_storage_snapshot() {
+    test_peer_states(UpdatingPeerStateWithStorageSnapshot).await;
 }
 
 #[derive(Debug)]
