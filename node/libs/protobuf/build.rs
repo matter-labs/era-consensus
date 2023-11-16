c613e45a08eba5e9558e7f4673f2157f2ed6c04f
Alex Ostrovski <aov@matterlabs.dev>
2023-11-16 10:25:46 +0200

    Move conformance test to example

--- node/libs/protobuf/build.rs
+++ node/libs/protobuf/build.rs
@@ -20,4 +20,14 @@ fn main() {
     }
     .generate()
     .expect("generate(test)");
+
+    zksync_protobuf_build::Config {
+        input_root: "examples/conformance_test/proto".into(),
+        proto_root: "zksync/protobuf/conformance_test".into(),
+        dependencies: vec![],
+        protobuf_crate: "::zksync_protobuf".parse().expect("protobuf_crate"),
+        is_public: false,
+    }
+    .generate()
+    .expect("generate(conformance)");
 }
