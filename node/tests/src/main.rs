653872aa735bb4a1e36ba03136419ab9c66d0d96
IAvecilla <iavecilla@fi.uba.ar>
2024-03-05 18:41:54 -0300

    Add first implementation of delay test

--- node/tests/src/main.rs
+++ node/tests/src/main.rs
@@ -116,6 +116,46 @@ pub async fn sanity_test(test_result: Arc<Mutex<TestResult>>) -> anyhow::Result<
     Ok(())
 }
 
+/// Sanity test for the RPC server.
+/// We use unwraps here because this function is intended to be used like a test.
+pub async fn delay_test(test_result: Arc<Mutex<TestResult>>) -> anyhow::Result<()> {
+    let client = k8s::get_client().await.unwrap();
+    let target_node = "consensus-node-01";
+    let ip = k8s::get_node_rpc_address_with_name(&client, target_node)
+        .await
+        .unwrap();
+    add_chaos_delay_for_node(&client, target_node)
+        .await
+        .unwrap();
+    let url: String = format!("http://{}", ip);
+    let rpc_client = HttpClientBuilder::default().build(url).unwrap();
+    let response: serde_json::Value = rpc_client
+        .request(last_vote::method(), rpc_params!())
+        .await
+        .unwrap();
+    let last_voted_view: u64 = serde_json::from_value(response).unwrap();
+    info!("last_voted_view: {}", last_voted_view);
+    for _ in 0..5 {
+        let response: serde_json::Value = rpc_client
+            .request(last_vote::method(), rpc_params!())
+            .await
+            .unwrap();
+        let new_last_voted_view: u64 = serde_json::from_value(response).unwrap();
+        info!("new_last_voted_view: {}", new_last_voted_view);
+        assert_eq!(new_last_voted_view, last_voted_view);
+    }
+    sleep(Duration::from_secs(10));
+    let response: serde_json::Value = rpc_client
+        .request(last_vote::method(), rpc_params!())
+        .await
+        .unwrap();
+    let new_last_voted_view: u64 = serde_json::from_value(response).unwrap();
+    info!("new_last_voted_view: {}", new_last_voted_view);
+    assert!(new_last_voted_view > last_voted_view);
+    test_result.lock().unwrap().add_passed();
+    Ok(())
+}
+
 /// Main function for the test.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -136,7 +176,10 @@ async fn main() -> anyhow::Result<()> {
                     args.rpc_port,
                     test_results.clone(),
                 ));
-                s.spawn(async { sanity_test(test_results.clone()).await });
+                s.spawn(async {
+                    sanity_test(test_results.clone()).await?;
+                    delay_test(test_results.clone()).await
+                });
                 Ok(())
             })
             .await
