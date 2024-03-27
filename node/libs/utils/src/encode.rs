d8fe5374126fa1976953cba1cee1874e90a8b5d0
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-03-27 16:37:05 +0100

    sample_opt

--- node/libs/utils/src/encode.rs
+++ node/libs/utils/src/encode.rs
@@ -18,11 +18,13 @@ impl EncodeDist {
     /// Returns a small non-empty range if `required_only` is false.
     /// Returns an empty range otherwise.
     pub fn sample_range(&self, rng: &mut (impl Rng + ?Sized)) -> std::ops::Range<usize> {
-        if self.required_only {
-            0..0
-        } else {
-            0..rng.gen_range(5..10)
-        }
+        if self.required_only { 0..0 } else { 0..rng.gen_range(5..10) }
+    }
+
+    /// Returns `Some(f())` if `required_only` is false.
+    /// Returns `None otherwise.
+    pub fn sample_opt<T>(&self, f: impl FnOnce()->T) -> Option<T> {
+        if self.required_only { None } else { Some(f()) }
     }
 }
 
@@ -42,15 +44,6 @@ impl Distribution<String> for EncodeDist {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> String {
         let n = self.sample_range(rng).len();
         Alphanumeric.sample_string(rng, n)
-    }
-}
-
-impl<T> Distribution<Option<T>> for EncodeDist
-where
-    EncodeDist: Distribution<T>,
-{
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Option<T> {
-        self.sample_range(rng).map(|_| self.sample(rng)).next()
     }
 }
 
