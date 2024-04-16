79b4dd9c4e3c57f586be6215d75213b9d76ca6ae
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-04-16 13:22:10 +0200

    added support for pipelining blocks in BlockStore

--- node/libs/storage/src/tests.rs
+++ node/libs/storage/src/tests.rs
@@ -1,10 +1,11 @@
 use super::*;
 use crate::{testonly::new_store_with_first, ReplicaState};
-use zksync_concurrency::{ctx, scope, testonly::abort_on_panic};
+use zksync_concurrency::{ctx, sync, scope, testonly::abort_on_panic};
 use zksync_consensus_roles::validator::testonly::Setup;
 
 #[tokio::test]
 async fn test_inmemory_block_store() {
+    abort_on_panic();
     let ctx = &ctx::test_root(&ctx::RealClock);
     let rng = &mut ctx.rng();
     let mut setup = Setup::new(rng, 3);
@@ -16,7 +17,8 @@ async fn test_inmemory_block_store() {
     );
     let mut want = vec![];
     for block in &setup.blocks {
-        store.store_next_block(ctx, block).await.unwrap();
+        store.queue_next_block(ctx, block.clone()).await.unwrap();
+        sync::wait_for(ctx, &mut store.persisted(), |p| p.contains(block.number())).await.unwrap();
         want.push(block.clone());
         assert_eq!(want, testonly::dump(ctx, store).await);
     }
@@ -41,7 +43,6 @@ async fn test_state_updates() {
     let (store, runner) = new_store_with_first(ctx, &setup.genesis, first_block.number()).await;
     scope::run!(ctx, |ctx, s| async {
         s.spawn_bg(runner.run(ctx));
-        let sub = &mut store.subscribe();
         let want = BlockStoreState {
             first: first_block.number(),
             last: None,
@@ -55,7 +56,7 @@ async fn test_state_updates() {
         ] {
             store.wait_until_queued(ctx, n).await.unwrap();
             store.wait_until_persisted(ctx, n).await.unwrap();
-            assert_eq!(want, *sub.borrow());
+            assert_eq!(want, store.available());
         }
 
         for block in &setup.blocks {
@@ -67,7 +68,7 @@ async fn test_state_updates() {
                     .wait_until_persisted(ctx, block.number())
                     .await
                     .unwrap();
-                assert_eq!(want, *sub.borrow());
+                assert_eq!(want, store.available());
             } else {
                 // Otherwise the state should be updated as soon as block is queued.
                 assert_eq!(
@@ -75,7 +76,7 @@ async fn test_state_updates() {
                         first: first_block.number(),
                         last: Some(block.justification.clone()),
                     },
-                    *sub.borrow()
+                    store.available()
                 );
             }
         }
