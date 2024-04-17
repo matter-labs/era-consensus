8f93e0708511f71885ba28ec4586fc7ef84a6977
ElFantasma <esteban.dimitroff@lambdaclass.com>
2024-04-17 11:37:17 -0300

    Weighted validators (BFT-408, BFT-424) (#77)
    ## What ❔
    
    Add weights to the validator set and change the consensus threshold
    rules to require >80% of weight.
    
    ## Why ❔
    
    We need for the L1 contracts to only accept blocks that were built by
    our node (which is the single leader). We can give more weight to our
    node, so that a QC isn't valid unless it signs. In turn this will be
    useful in the future

--- node/actors/bft/src/tests.rs
+++ node/actors/bft/src/tests.rs
@@ -8,9 +8,12 @@ async fn run_test(behavior: Behavior, network: Network) {
     let ctx = &ctx::test_root(&ctx::RealClock);
 
     const NODES: usize = 11;
-    let mut nodes = vec![behavior; NODES];
-    for n in &mut nodes[0..validator::threshold(NODES)] {
-        *n = Behavior::Honest;
+    let mut nodes = vec![(behavior, 1u64); NODES];
+    // validator::threshold(NODES) will calculate required nodes to validate a message
+    // given each node weight is 1
+    let honest_nodes_amount = validator::threshold(NODES as u64) as usize;
+    for n in &mut nodes[0..honest_nodes_amount] {
+        n.0 = Behavior::Honest;
     }
     Test {
         network,
@@ -185,7 +188,7 @@ async fn non_proposing_leader() {
     let ctx = &ctx::test_root(&ctx::AffineClock::new(5.));
     Test {
         network: Network::Real,
-        nodes: vec![Behavior::Honest, Behavior::HonestNotProposing],
+        nodes: vec![(Behavior::Honest, 1), (Behavior::HonestNotProposing, 1)],
         blocks_to_finalize: 10,
     }
     .run(ctx)
--- node/libs/roles/src/validator/messages/leader_prepare.rs
+++ node/libs/roles/src/validator/messages/leader_prepare.rs
@@ -30,13 +30,13 @@ pub enum PrepareQCVerifyError {
     /// Bad message format.
     #[error(transparent)]
     BadFormat(anyhow::Error),
-    /// Not enough signers.
-    #[error("not enough signers: got {got}, want {want}")]
+    /// Weight not reached.
+    #[error("Signers have not reached threshold weight: got {got}, want {want}")]
     NotEnoughSigners {
-        /// Got signers.
-        got: usize,
-        /// Want signers.
-        want: usize,
+        /// Got weight.
+        got: u64,
+        /// Want weight.
+        want: u64,
     },
     /// Bad signature.
     #[error("bad signature: {0:#}")]
@@ -56,14 +56,14 @@ impl PrepareQC {
     /// Get the highest block voted and check if there's a quorum of votes for it. To have a quorum
     /// in this situation, we require 2*f+1 votes, where f is the maximum number of faulty replicas.
     pub fn high_vote(&self, genesis: &Genesis) -> Option<BlockHeader> {
-        let mut count: HashMap<_, usize> = HashMap::new();
+        let mut count: HashMap<_, u64> = HashMap::new();
         for (msg, signers) in &self.map {
             if let Some(v) = &msg.high_vote {
-                *count.entry(v.proposal).or_default() += signers.count();
+                *count.entry(v.proposal).or_default() += genesis.validators.weight(signers);
             }
         }
         // We only take one value from the iterator because there can only be at most one block with a quorum of 2f+1 votes.
-        let min = 2 * genesis.validators.faulty_replicas() + 1;
+        let min = 2 * genesis.validators.max_faulty_weight() + 1;
         count.into_iter().find(|x| x.1 >= min).map(|x| x.0)
     }
 
@@ -101,6 +101,7 @@ impl PrepareQC {
     pub fn verify(&self, genesis: &Genesis) -> Result<(), PrepareQCVerifyError> {
         use PrepareQCVerifyError as Error;
         let mut sum = Signers::new(genesis.validators.len());
+
         // Check the ReplicaPrepare messages.
         for (i, (msg, signers)) in self.map.iter().enumerate() {
             if msg.view != self.view {
@@ -126,11 +127,12 @@ impl PrepareQC {
             sum |= signers;
         }
 
-        // Verify that we have enough signers.
+        // Verify the signers' weight is enough.
+        let weight = genesis.validators.weight(&sum);
         let threshold = genesis.validators.threshold();
-        if sum.count() < threshold {
+        if weight < threshold {
             return Err(Error::NotEnoughSigners {
-                got: sum.count(),
+                got: weight,
                 want: threshold,
             });
         }
@@ -138,7 +140,7 @@ impl PrepareQC {
         let messages_and_keys = self.map.clone().into_iter().flat_map(|(msg, signers)| {
             genesis
                 .validators
-                .iter()
+                .iter_keys()
                 .enumerate()
                 .filter(|(i, _)| signers.0[*i])
                 .map(|(_, pk)| (msg.clone(), pk))
@@ -148,6 +150,14 @@ impl PrepareQC {
         self.signature
             .verify_messages(messages_and_keys)
             .map_err(Error::BadSignature)
+    }
+
+    /// Calculates the weight of current PrepareQC signing validators
+    pub fn weight(&self, committee: &validator::Committee) -> u64 {
+        self.map
+            .values()
+            .map(|signers| committee.weight(signers))
+            .sum()
     }
 }
 
