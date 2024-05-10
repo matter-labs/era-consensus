8168af734f7e9a8498a972b069ef8b0c197af659
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-05-10 11:14:10 -0300

    Add trait for the state keeper

--- node/actors/network/src/lib.rs
+++ node/actors/network/src/lib.rs
@@ -6,6 +6,7 @@ use zksync_concurrency::{
     ctx::{self, channel},
     limiter, scope,
 };
+use zksync_consensus_roles::attester;
 use zksync_consensus_storage::BlockStore;
 use zksync_consensus_utils::pipe::ActorPipe;
 
@@ -28,6 +29,20 @@ mod tests;
 mod watch;
 
 pub use config::*;
+
+/// Trait for the shared state of batches between the consensus and the execution layer.
+pub trait StateKeeper {
+    /// Get the L1 batch from storage with the highest number.
+    fn last_batch(&self) -> attester::BatchNumber;
+    /// Get the L1 batch QC from storage with the highest number.
+    fn last_batch_qc(&self) -> attester::BatchNumber;
+    /// Returns the batch with the given number.
+    fn get_batch(&self, number: attester::BatchNumber) -> Option<attester::Batch>;
+    /// Returns the QC of the batch with the given number.
+    fn get_batch_qc(&self, number: attester::BatchNumber) -> Option<attester::BatchQC>;
+    /// Store the given QC in the storage.
+    fn store_qc(&self, qc: attester::BatchQC);
+}
 
 /// State of the network actor observable outside of the actor.
 pub struct Network {
