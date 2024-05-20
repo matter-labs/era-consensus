d84421c6f5c437489ffb93b94b0eacfd8f3a317e
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-05-20 11:29:10 -0300

    Fix last comment in gossip runner

--- node/actors/network/src/gossip/runner.rs
+++ node/actors/network/src/gossip/runner.rs
@@ -113,13 +113,6 @@ impl Network {
             ctx,
             self.cfg.rpc.push_block_store_state_rate,
         );
-        let mut push_signature_client: Option<rpc::Client<rpc::push_batch_votes::Rpc>> = None;
-        let mut push_signature_server: Option<PushBatchVotesServer> = None;
-        if self.genesis().attesters.is_some() {
-            push_signature_client =
-                rpc::Client::new(ctx, self.cfg.rpc.push_batch_votes_rate).into();
-            push_signature_server = PushBatchVotesServer(self).into();
-        };
         let push_block_store_state_server = PushBlockStoreStateServer::new(self);
         let get_block_client =
             rpc::Client::<rpc::get_block::Rpc>::new(ctx, self.cfg.rpc.get_block_rate);
@@ -140,12 +133,34 @@ impl Network {
                 .add_client(&get_block_client)
                 .add_server(ctx, &*self.block_store, self.cfg.rpc.get_block_rate)
                 .add_server(ctx, rpc::ping::Server, rpc::ping::RATE);
-            if let (Some(client), Some(server)) = (&push_signature_client, push_signature_server) {
-                service = service.add_client(client).add_server(
+            if self.genesis().attesters.as_ref().is_some() {
+                let push_signature_client = rpc::Client::<rpc::push_batch_votes::Rpc>::new(
                     ctx,
-                    server,
                     self.cfg.rpc.push_batch_votes_rate,
                 );
+                let push_signature_server = PushBatchVotesServer(self);
+                service = service.add_client(&push_signature_client).add_server(
+                    ctx,
+                    push_signature_server,
+                    self.cfg.rpc.push_batch_votes_rate,
+                );
+                // Push L1 batch votes updates to peer.
+                s.spawn::<()>(async {
+                    let push_signature_client = push_signature_client;
+                    let mut old = BatchVotes::default();
+                    let mut sub = self.batch_votes.subscribe();
+                    sub.mark_changed();
+                    loop {
+                        let new = sync::changed(ctx, &mut sub).await?.clone();
+                        let diff = new.get_newer(&old);
+                        if diff.is_empty() {
+                            continue;
+                        }
+                        old = new;
+                        let req = rpc::push_batch_votes::Req(diff);
+                        push_signature_client.call(ctx, &req, kB).await?;
+                    }
+                });
             }
 
             if let Some(ping_timeout) = &self.cfg.ping_timeout {
@@ -186,25 +201,6 @@ impl Network {
                     push_validator_addrs_client.call(ctx, &req, kB).await?;
                 }
             });
-
-            if let Some(client) = &push_signature_client {
-                // Push L1 batch votes updates to peer.
-                s.spawn::<()>(async {
-                    let mut old = BatchVotes::default();
-                    let mut sub = self.batch_votes.subscribe();
-                    sub.mark_changed();
-                    loop {
-                        let new = sync::changed(ctx, &mut sub).await?.clone();
-                        let diff = new.get_newer(&old);
-                        if diff.is_empty() {
-                            continue;
-                        }
-                        old = new;
-                        let req = rpc::push_batch_votes::Req(diff);
-                        client.call(ctx, &req, kB).await?;
-                    }
-                });
-            };
 
             // Perform get_block calls to peer.
             s.spawn::<()>(async {
