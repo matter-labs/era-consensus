800b01b040370ee8ca814a216556364aaeb40034
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-05-28 15:39:17 -0300

    Update implementation for the BatchStore to persist and queue new batches

--- node/libs/storage/src/batch_store.rs
+++ node/libs/storage/src/batch_store.rs
@@ -1,36 +1,10 @@
 //! Defines storage layer for batches of blocks.
 use anyhow::Context as _;
+use std::collections::VecDeque;
 use std::{fmt, sync::Arc};
 use zksync_concurrency::{ctx, scope, sync};
 use zksync_consensus_roles::attester;
 use zksync_consensus_roles::validator;
-
-/// Trait for the shared state of batches between the consensus and the execution layer.
-#[async_trait::async_trait]
-pub trait PersistentBatchStore: 'static + fmt::Debug + Send + Sync {
-    /// Get the L1 batch from storage with the highest number.
-    fn last_batch(&self) -> attester::BatchNumber;
-    /// Get the L1 batch QC from storage with the highest number.
-    fn last_batch_qc(&self) -> attester::BatchQC;
-    /// Returns the batch with the given number.
-    fn get_batch(&self, number: attester::BatchNumber) -> Option<attester::FinalBatch>;
-    /// Returns the QC of the batch with the given number.
-    fn get_batch_qc(&self, number: attester::BatchNumber) -> Option<attester::BatchQC>;
-    /// Store the given QC in the storage.
-    fn store_qc(&self, qc: attester::BatchQC);
-    /// Range of batches persisted in storage.
-    fn persisted(&self) -> sync::watch::Receiver<BatchStoreState>;
-    /// Queue the batch to be persisted in storage.
-    /// `queue_next_batch()` may return BEFORE the batch is actually persisted,
-    /// but if the call succeeded the batch is expected to be persisted eventually.
-    /// Implementations are only required to accept a batch directly after the previous queued
-    /// batch, starting with `persisted().borrow().next()`.
-    async fn queue_next_batch(
-        &self,
-        ctx: &ctx::Ctx,
-        batch: attester::FinalBatch,
-    ) -> ctx::Result<()>;
-}
 
 /// State of the `BatchStore`: continuous range of batches.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -74,6 +48,33 @@ impl BatchStoreState {
     }
 }
 
+/// Trait for the shared state of batches between the consensus and the execution layer.
+#[async_trait::async_trait]
+pub trait PersistentBatchStore: 'static + fmt::Debug + Send + Sync {
+    /// Get the L1 batch from storage with the highest number.
+    fn last_batch(&self) -> attester::BatchNumber;
+    /// Get the L1 batch QC from storage with the highest number.
+    fn last_batch_qc(&self) -> attester::BatchQC;
+    /// Returns the batch with the given number.
+    fn get_batch(&self, number: attester::BatchNumber) -> Option<attester::FinalBatch>;
+    /// Returns the QC of the batch with the given number.
+    fn get_batch_qc(&self, number: attester::BatchNumber) -> Option<attester::BatchQC>;
+    /// Store the given QC in the storage.
+    fn store_qc(&self, qc: attester::BatchQC);
+    /// Range of batches persisted in storage.
+    fn persisted(&self) -> sync::watch::Receiver<BatchStoreState>;
+    /// Queue the batch to be persisted in storage.
+    /// `queue_next_batch()` may return BEFORE the batch is actually persisted,
+    /// but if the call succeeded the batch is expected to be persisted eventually.
+    /// Implementations are only required to accept a batch directly after the previous queued
+    /// batch, starting with `persisted().borrow().next()`.
+    async fn queue_next_batch(
+        &self,
+        ctx: &ctx::Ctx,
+        batch: attester::FinalBatch,
+    ) -> ctx::Result<()>;
+}
+
 /// Inner state of the `BatchStore`.
 #[derive(Debug)]
 struct Inner {
@@ -81,9 +82,17 @@ struct Inner {
     queued: BatchStoreState,
     /// Batches that are already persisted.
     persisted: BatchStoreState,
+    cache: VecDeque<attester::FinalBatch>,
 }
 
 impl Inner {
+    /// Minimal number of most recent batches to keep in memory.
+    /// It allows to serve the recent batches to peers fast, even
+    /// if persistent storage reads are slow (like in RocksDB).
+    /// `BatchStore` may keep in memory more batches in case
+    /// batches are queued faster than they are persisted.
+    const CACHE_CAPACITY: usize = 10;
+
     /// Tries to push the next batch to cache.
     /// Noop if provided batch is not the expected one.
     /// Returns true iff cache has been modified.
@@ -92,7 +101,24 @@ impl Inner {
             return false;
         }
         self.queued.last = Some(batch.justification.clone());
+        self.cache.push_back(batch.clone());
+        self.truncate_cache();
         true
+    }
+
+    fn truncate_cache(&mut self) {
+        while self.cache.len() > Self::CACHE_CAPACITY
+            && self.persisted.contains(self.cache[0].number())
+        {
+            self.cache.pop_front();
+        }
+    }
+
+    fn batch(&self, n: attester::BatchNumber) -> Option<attester::FinalBatch> {
+        // Subtraction is safe, because batches in cache are
+        // stored in increasing order of batch number.
+        let first = self.cache.front()?;
+        self.cache.get((n.0 - first.number().0) as usize).cloned()
     }
 }
 
@@ -113,22 +139,27 @@ impl BatchStoreRunner {
         let res = scope::run!(ctx, |ctx, s| async {
             let persisted = self.0.persistent.persisted();
             let mut queue_next = persisted.borrow().next();
-            // Task queueing batches to be persisted.
-            let inner = &mut self.0.inner.subscribe();
+            // Task truncating cache whenever a batch gets persisted.
             s.spawn::<()>(async {
                 let mut persisted = persisted;
                 loop {
                     let persisted = sync::changed(ctx, &mut persisted).await?.clone();
                     self.0.inner.send_modify(|inner| {
                         inner.persisted = persisted;
+                        inner.truncate_cache();
                     });
                 }
             });
+            let inner = &mut self.0.inner.subscribe();
             loop {
-                sync::wait_for(ctx, inner, |inner| inner.queued.contains(queue_next)).await?;
-                let batch = self.0.batch(ctx, queue_next).await?.context("batch()")?;
-                self.0.persistent.queue_next_batch(ctx, batch).await?;
+                let batch = sync::wait_for(ctx, inner, |inner| inner.queued.contains(queue_next))
+                    .await?
+                    .batch(queue_next)
+                    .unwrap();
+
                 queue_next = queue_next.next();
+
+                self.0.persistent.queue_next_batch(ctx, batch).await?;
             }
         })
         .await;
@@ -155,6 +186,7 @@ impl BatchStore {
             inner: sync::watch::channel(Inner {
                 queued: persisted.clone(),
                 persisted,
+                cache: VecDeque::new(),
             })
             .0,
             persistent,
@@ -177,6 +209,9 @@ impl BatchStore {
         if !inner.queued.contains(number) {
             return Ok(None);
         }
+        if let Some(batch) = inner.batch(number) {
+            return Ok(Some(batch));
+        }
         let batch = self
             .persistent
             .get_batch(number)
@@ -197,11 +232,15 @@ impl BatchStore {
         genesis: validator::Genesis,
     ) -> ctx::Result<()> {
         batch.verify(&genesis).context("batch.verify()")?;
+
         sync::wait_for(ctx, &mut self.inner.subscribe(), |inner| {
             inner.queued.next() >= batch.number()
         })
         .await?;
-        self.inner.send_if_modified(|inner| inner.try_push(batch));
+
+        self.inner
+            .send_if_modified(|inner| inner.try_push(batch.clone()));
+
         Ok(())
     }
 
