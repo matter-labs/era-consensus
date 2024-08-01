dfda9a905737fae9570d105df0321b8269cf3afc
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-08-01 17:11:09 +0200

    wip

--- node/actors/network/src/gossip/attestation_status.rs
+++ node/actors/network/src/gossip/attestation_status.rs
@@ -2,12 +2,13 @@ use std::fmt;
 use std::sync::Arc;
 use zksync_concurrency::sync;
 use zksync_consensus_roles::{attester};
+use std::collections::HashSet;
 
 use crate::watch::Watch;
 
-/// Coordinate the attestation by showing the status as seen by the main node.
+/// Coordinate the attestation by showing the config as seen by the main node.
 #[derive(Debug, Clone)]
-pub struct AttestationStatus {
+pub struct AttestationConfig {
     pub batch_to_attest: attester::Batch,
     /// Committee for that batch.
     /// NOTE: the committee is not supposed to change often,
@@ -19,17 +20,29 @@ pub struct AttestationStatus {
 /// A [Watch] over an [AttestationStatus] which we can use to notify components about
 /// changes in the batch number the main node expects attesters to vote on.
 struct Inner {
-    status: Arc<AttestationStatus>,
+    config: Arc<AttestationConfig>,
     votes: im::HashMap<attester::PublicKey, Arc<attester::Signed<attester::Batch>>>,
     weight: attester::Weight,
 }
 
 impl Inner {
+    /// Returns a set of votes of `self` which are newer than the entries in `old`.
+    fn new_votes(&self, old: &Self) -> Vec<Arc<attester::Signed<attester::Batch>>> {
+        match self.config.batch_to_attest.number.cmp(old.config.batch_to_attest.number) {
+            Ordering::Less => vec![],
+            Ordering::Greater => self.votes.values().cloned().collect(),
+            Ordering::Equal => self.votes.iter()
+                .filter(|(k,_)|!old.contains(k))
+                .map(|(_,v)|v.clone())
+                .collect()
+        }
+    }
+
     /// Verifies and adds a vote.
     fn insert_vote(&mut self, vote: Arc<attester::Signed<attester::Batch>>) -> anyhow::Result<()> {
-        anyhow::ensure(vote.msg.genesis == self.status.batch_to_attest.genesis, "Genesis mismatch");
-        if vote.msg != self.status.batch_to_attest { return Ok(()) }
-        let Some(weight) = self.status.committee.contains(&vote.key) else { return Ok(()) };
+        anyhow::ensure(vote.msg.genesis == self.config.batch_to_attest.genesis, "Genesis mismatch");
+        if vote.msg != self.config.batch_to_attest { return Ok(()) }
+        let Some(weight) = self.config.committee.contains(&vote.key) else { return Ok(()) };
         if self.votes.contains(&vote.key) { return Ok(()) }
         vote.verify().context("verify")?;
         self.votes.insert(vote.key.clone(),vote);
@@ -46,63 +59,67 @@ impl Inner {
             if done.contains(&vote.key) {
                 anyhow::bail!("duplicate entry for {:?}", vote.key);
             }
-            done.insert(&vote.key);
+            done.insert(vote.key.clone());
             self.insert_vote(vote);
         }
         Ok(())
     }
 }
 
-struct AttestationStatusWatch(watch::Sender<Inner>);
+pub struct AttestationState {
+    key: Option<attester::SecretKey>,
+    inner: Watch<Inner>,
+}
 
-impl AttestationStatusWatch {
+impl AttestationState {
     /// Constructs AttestationStatusWatch.
-    pub fn new(s: AttestationStatus) -> Self {
-        Self(Watch::new(Inner {
-            status: s.into(),
-            votes: [].into(),
-            weight: 0,
-        }))
+    pub fn new(s: AttestationConfig, key: Option<attester::SecretKey>) -> Self {
+        Self {
+            key,
+            inner: Watch::new(Inner {
+                config: s.into(),
+                votes: [].into(),
+                weight: 0,
+            }),
+        }
     }
 
-    pub fn insert_votes(&mut self, votes: impl Iterator<Arc<attester::Signer<attester::Batch>>>) -> anyhow::Result<()> {
-        let mut res;
-        self.0.send_if_modified(|this| {
-            let before = this.weight;
-            res = this.insert_votes(votes);
-            this.weight > before
-        });
+    pub async fn insert_votes(&mut self, votes: impl Iterator<Arc<attester::Signer<attester::Batch>>>) -> anyhow::Result<()> {
+        let this = self.inner.lock().await;
+        let mut inner = this.borrow().clone();
+        let before = inner.weight;
+        let res = inner.insert_votes(votes);
+        if inner.weight > before {
+            this.send_replace(inner);
+        }
         res
     }
 
-    pub async fn wait_for_qc(&mut self, ctx: &ctx::Ctx) -> ctx::OrCanceled<attester::BatchQC> {
-        let sub = &mut self.0.subscribe();
-        let t = sub.borrow().status.committee.threshold();
+    pub async fn wait_for_qc(&self, ctx: &ctx::Ctx) -> ctx::OrCanceled<attester::BatchQC> {
+        let sub = &mut self.inner.subscribe();
+        let t = sub.borrow().config.committee.threshold();
         let this = sync::wait_for(ctx, sub, |this| this.weight >= t).await?;
         let mut sigs = attester::MultiSig::default();
         for vote in this.votes.values() {
             sigs.add(vote.key.clone(),vote.sig.clone());
         }
         Ok(attester::BatchQC {
-            message: this.status.batch_to_attest.clone(),
+            message: this.config.batch_to_attest.clone(),
             signatures: sigs,
         })
     }
 
-    /// Subscribes to AttestationStatus updates.
-    pub fn subscribe(&self) -> AttestationStatusReceiver {
-        self.0.subscribe()
-    } 
-
-    /// Set the next batch number to attest on and notify subscribers it changed.
-    /// Returns an error if the update it not valid.
-    pub fn update(&self, s: AttestationStatus) -> anyhow::Result<()> {
-        sync::try_send_modify(&self.0, |this| {
-            anyhow::ensure!(this.status.batch_to_attest.genesis == s.batch_to_attest.genesis, "tried to change genesis");
-            anyhow::ensure!(this.status.batch_to_attest.message.number < s.batch_to_attest.message.number, "tried to decrease batch number");
-            this.status = s;
+    pub async fn advance(&self, s: AttestationConfig) -> anyhow::Result<()> {
+        let vote = self.key.as_ref().map(|key| key.sign_msg(s.batch_to_attest.clone().insert())); 
+        sync::try_send_modify(&self.inner.lock().await, |this| {
+            anyhow::ensure!(this.config.batch_to_attest.genesis == s.batch_to_attest.genesis, "tried to change genesis");
+            anyhow::ensure!(this.config.batch_to_attest.message.number < s.batch_to_attest.message.number, "tried to decrease batch number");
+            this.config = Arc::new(s);
             this.votes.clear();
             this.weight = 0;
+            if let Some(vote) = vote {
+                this.insert_vote(Arc::new(vote)).unwrap();
+            }
             Ok(())
         })
     }
