dd411587788f7ea5a64ee3bcbb270e093e1182b1
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-08-05 12:46:25 +0200

    bumped capability ID of the push_batch_votes rpc (#176)
    We added an extra field to the Batch message, without considering the
    fact that an extra field won't be recognized by old binaries and they
    will fail to verify signatures on Batch messages. They will consider
    sending such invalid signatures malicious and will disconnect from the
    peer. To avoid this we create a new RPC instead (by just bumping the
    capability id on push_batch_votes RPC) which will not be interpreted by
    the old binaries at all.

--- node/actors/network/src/gossip/mod.rs
+++ node/actors/network/src/gossip/mod.rs
@@ -9,14 +9,15 @@
 //! * static connections (explicitly declared in configs of both ends of the connection).
 //! * dynamic connections (additional randomized connections which are established to improve
 //!   the throughput of the network).
+//!
 //! Static connections constitute a rigid "backbone" of the gossip network, which is insensitive to
 //! eclipse attack. Dynamic connections are supposed to improve the properties of the gossip
 //! network graph (minimize its diameter, increase connectedness).
-pub use self::attestation_status::{
-    AttestationStatus, AttestationStatusReceiver, AttestationStatusWatch,
-};
-pub use self::batch_votes::BatchVotesPublisher;
 use self::batch_votes::BatchVotesWatch;
+pub use self::{
+    attestation_status::{AttestationStatus, AttestationStatusReceiver, AttestationStatusWatch},
+    batch_votes::BatchVotesPublisher,
+};
 use crate::{gossip::ValidatorAddrsWatch, io, pool::PoolWatch, Config, MeteredStreamStats};
 use fetch::RequestItem;
 use std::sync::{atomic::AtomicUsize, Arc};
