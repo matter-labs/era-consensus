f627a3c6b367da98287ee9363e136975fb19f4cf
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-08-13 13:31:05 +0200

    unused file

--- node/actors/executor/src/attestation.rs
+++
@@ -1,5 +0,0 @@
-//! Module to publish attestations over batches.
-use anyhow::Context as _;
-use std::sync::Arc;
-use zksync_concurrency::{ctx, time};
-pub use zksync_consensus_network::gossip::attestation::*;
