f625fefc4f2313722af838b0e3f0e93176caa7d1
Bruno França <bruno@franca.xyz>
2025-02-01 16:06:20 +0000

    clippy

--- node/components/bft/src/chonky_bft/proposer.rs
+++ node/components/bft/src/chonky_bft/proposer.rs
@@ -1,4 +1,4 @@
-use crate::{metrics, Config, ToNetworkMessage};
+use crate::{Config, ToNetworkMessage};
 use std::sync::Arc;
 use zksync_concurrency::{ctx, error::Wrap as _, sync};
 use zksync_consensus_network::io::ConsensusInputMessage;
