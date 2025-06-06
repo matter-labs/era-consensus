12dc40b42c2baf7ca998cfa26b5afc2e3987b40e
Bruno França <bruno@franca.xyz>
2025-06-06 14:29:00 +0100

    remove docker/k8s code

--- infrastructure/loadtests/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = var.project_id
-}
-
-provider "random" {}
--- node/tests/tester_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This file works as an entrypoint of the kubernetes cluster running the tester binary copied inside of it.
-
-export RUST_LOG=INFO
-./tester run
